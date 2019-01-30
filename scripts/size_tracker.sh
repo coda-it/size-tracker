e89dfd6a057d3107ccbb18a2fe9a44ea8bdfa054
Oskar Szura <oskar.szura@gmail.com>
2019-01-30 21:17:19 +0100

    SITR-1 - Create basic functionality

---
+++ scripts/size_tracker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NEWER_TAG=$(git tag --sort version:refname | tail -r | sed -n 1p)
+OLDER_TAG=$(git tag --sort version:refname | tail -r | sed -n 2p)
+HEADER="# Sizelog from $NEWER_TAG"
+DIR=./docs/sizelogs
+
+if [ ! -d $DIR ]
+    then
+    mkdir -p $DIR
+fi
+
+SIZEFILE=$DIR/CHANGELOG_$NEWER_TAG.md
+
+[ -f size_report.txt ] && rm size_report.txt
+
+echo $HEADER > $SIZEFILE
+echo "### Sizefiles" >> $SIZEFILE
+
+while IFS=";" read -r p || [ -n "$p" ]
+do
+  FILE=$(echo $p | cut -d ";" -f 1)
+  SIZE=$(du -h $FILE)
+  echo $SIZE >> $SIZEFILE
+done < .size_tracker
